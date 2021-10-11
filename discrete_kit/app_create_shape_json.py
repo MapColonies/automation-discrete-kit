57276b23ff0ddc8d39764ac432d1f081bfe32200
dimitry <dimitrys@rafael.co.il>
2021-10-11 16:19:11 +0300

    fix(automation):
    + Linux folder path.

--- discrete_kit/app_create_shape_json.py
+++ discrete_kit/app_create_shape_json.py
@@ -11,11 +11,12 @@ This is an example of creating json from shape file.
 """
 if __name__ == '__main__':
     c = CreateJsonShape(r'/home/dimitry/Downloads/2021-2686-0')  # Created with None
-    print(c.get_json_output())
-    # try:
-    #     with open(Path(Path(__file__).resolve()).parent.parent / 'jsons/shape_file.json', 'w', encoding='utf-8') as f:
-    #         json.dump(json.loads(c.get_json_output()), f, ensure_ascii=False)
-    # except IOError:
-    #     raise Exception("Cannot write json file")
-    # schema_validator.validate_json_types(c.get_json_output())
+    # c.add_ext_source_name()
+    # print(c.get_json_output())
+    try:
+        with open(Path(Path(__file__).resolve()).parent / 'jsons/shape_file.json', 'w', encoding='utf-8') as f:
+            json.dump(json.loads(c.get_json_output()), f, ensure_ascii=False)
+    except IOError:
+        raise Exception("Cannot write json file")
+    schema_validator.validate_json_types(c.get_json_output())
     # json_compare_pycsw()

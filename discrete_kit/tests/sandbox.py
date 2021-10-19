c9f1b3dfb467306947ac63c868468c285b921ad5
ronenk1 <ronenk1@rnd-hub.com>
2021-10-19 16:58:27 +0300

    feat(automation): adding tfw update resolution generic function:  - metadata_convertor.py include:    * function that get file directory and value and update the file with the value    * function the recursively find all files with specific extension (tfw) and change with the first function the values on this files  - tests sub-package for unittests and sandbox.py

---
+++ discrete_kit/tests/sandbox.py
@@ -0,0 +1,10 @@
+"""This  module is for testing function on developing"""
+
+from discrete_kit.functions import metadata_convertor as ms
+
+import os
+import glob
+directory = '/home/ronenk1/Downloads/testing/1/'
+glob.glob('./*.*')
+# ms.change_resolution_value('test12345', '/home/ronenk1/Downloads/testing/1/tiff/O_arzi_mz_w84geo_Apr19_tiff_0.2.tfw')
+ms.replace_discrete_resolution(directory, 'test123')

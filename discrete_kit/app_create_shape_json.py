d0c95b060f6cde63074c49e5f3a137ba41179e44
dimitry <dimitrys@rafael.co.il>
2021-10-20 10:12:05 +0300

    fix(automation):
    + Fixed path in class

--- discrete_kit/app_create_shape_json.py
+++ discrete_kit/app_create_shape_json.py
@@ -12,9 +12,9 @@ This is an example of creating json from shape file.
 if __name__ == '__main__':
 
     # path_to_check = sys.argv[1] #ToDo : Added from CMD ?
-    shape_json = [x[0] for x in os.walk(config.PATH_TO_CHECK)]
-    shape_path = ("\n".join(s for s in shape_json if 'Shape'.lower() in s.lower()))
-    shape_json = ShapeToJSON(shape_path)  # Created with None
+    # shape_json = [x[0] for x in os.walk(config.PATH_TO_CHECK)]
+    # shape_path = ("\n".join(s for s in shape_json if 'Shape'.lower() in s.lower()))
+    shape_json = ShapeToJSON(config.PATH_TO_CHECK)  # Created with None
     # c.add_ext_source_name(path_to_check, ".shp", True)
     print(shape_json.get_json_output())
     try:

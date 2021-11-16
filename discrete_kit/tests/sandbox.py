48cf3d2b282f1f2742dc35d101cf5b23070e7690
dimitry <dimitrys@rafael.co.il>
2021-11-16 11:21:14 +0200

    fix(automation):
    + versioned problem

--- discrete_kit/tests/sandbox.py
+++ discrete_kit/tests/sandbox.py
@@ -8,3 +8,5 @@ directory = '/home/ronenk1/Downloads/testing/1/'
 glob.glob('./*.*')
 # ms.change_resolution_value('test12345', '/home/ronenk1/Downloads/testing/1/tiff/O_arzi_mz_w84geo_Apr19_tiff_0.2.tfw')
 ms.replace_discrete_resolution(directory, 'test123')
+
+# some temporary note change
